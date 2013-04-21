43c2514ae5387fc2fc79e75c0aa5e964b60afb86
Sunil Abraham <s.abraham@gmail.com>
2013-04-21 02:31:56 -0700

    HUGE changes - scored game tree, removed terminal nodes
    scoring of game tree - average of possible win states
    -- todo: move to minimax (but will just get draws...)
       sol?: 1- have minimax, and revert to average if tie,
             2- choose whatever if first, in event of tie

--- game.c
+++ game.c
@@ -31,15 +31,12 @@ struct stack_node {
 struct stack_node * init_stack () {
   struct stack_node * head = (struct stack_node *)
     malloc(sizeof(struct stack_node));
-  struct stack_node * z = (struct stack_node *)
-    malloc(sizeof(struct stack_node));
-  head->next = z;
-  z->next = z;
+  head->next = 0;
   return head;
 }
 
 bool stack_is_empty (struct stack_node *head) {
-  return head->next == head->next->next;
+  return head->next == 0;
 }
 
 struct node * pop (struct stack_node *head) {
@@ -120,9 +117,7 @@ bool endgame (unsigned int *p1, unsigned int *p2) {
 
 struct node * init_game_tree () {
   struct node *head = (struct node *) malloc(sizeof(struct node));
-  struct node *z = (struct node *) malloc(sizeof(struct node));
-  z->child = z; z->sibling = z;
-  head->child = z; head->sibling = head;
+  head->child = 0; head->sibling = 0;
   return head;
 }
 
@@ -154,17 +149,12 @@ void push_possible_moves (struct stack_node *head_sn,
       child->scored = false;
       child->mine = *p2; child->yours = *p1; // swap players each level
       move(&(child->mine), &i);
-      child_n->s = child;
+      child_n->s = child; child_n->child = 0; child_n->sibling = 0;
       child_sn->key = child_n;
-      if (prev_child_n) {
-        child_n->sibling = prev_child_n->sibling; // ie, point to z
+      if (prev_child_n)
         prev_child_n->sibling = child_n;
-      }
-      else {
-        struct node * z_n = (struct node *) malloc(sizeof(struct node));
-        z_n->child = z_n; z_n->sibling = z_n;
+      else
         parent_n->child = child_n;
-      }
       prev_child_n = child_n;
       //      printf("%d is a valid move!\n", i);
       push(head_sn, child_n);
@@ -178,96 +168,61 @@ double score (int *p1, int *p2) {
   else return 0.0;
 }
 
+double score_children (struct node *p) {
+  double sum = 0;
+  int count = 0;
+  struct node * c = p->child;
+  for (; c != 0; c = c->sibling, ++count) {
+    sum += c->s->score;
+  }
+  return sum / (double) count;
+}
+
 struct node * build_game_tree (int *p1, int *p2) {
   struct node *head = init_game_tree();
   struct state *state = (struct state *) malloc(sizeof(struct state));
   state->mine = *p1; state->yours = *p2;
   head->s = state;
-  struct node * z_n = (struct node *) malloc(sizeof(struct node));
-  z_n->child = z_n; z_n->sibling = z_n;
   struct stack_node *stack_head = init_stack();
+  struct stack_node *upper_stack_head = init_stack();
   struct node *popped;
   bool possible_moves[9];
   push(stack_head, head);
   while(!stack_is_empty(stack_head)) {
     popped = pop(stack_head);
     //    print_board(&(popped->s->mine), &(popped->s->yours));
-    if (!endgame(&(popped->s->mine), &(popped->s->yours)))
+    if (!endgame(&(popped->s->mine), &(popped->s->yours))) {
       push_possible_moves(stack_head, popped,
                           &(popped->s->mine), &(popped->s->yours));
+      push(upper_stack_head, popped);
+    }
     else {
-      popped->child = z_n;
+      popped->child = 0;
       popped->s->score =
         score(&(popped->s->mine), &(popped->s->yours));
       popped->s->scored = true;
       //      printf("score: %f\n", popped->s->score);
     }
   }
+  while(!stack_is_empty(upper_stack_head)) {
+    popped = pop(upper_stack_head);
+    popped->s->score = score_children(popped);
+    print_board(&(popped->s->mine), &(popped->s->yours));
+    printf("score: %f\n", popped->s->score);
+  }
   return head;
-}
-
-bool children_scored (struct node *p) {
-  printf("** in children_scored **");
-  struct node * c = p->child;
-  for (; c != c->sibling; c = c->sibling) {
-    if (!c->s->scored) return false;
-  }
-  return true;
-}
-
-double score_children (struct node *p) {
-  double sum;
-  int count;
-  struct node * c = p->child;
-  for (; c != c->sibling; c = c->sibling, ++count) {
-    sum += c->s->score;
-  }
-  return sum / (double) count;
-}
-
-void push_children (struct stack_node *head, struct node *p) {
-  struct node *c = p->child;
-  for (; c != c->sibling; c = c->sibling) {
-    printf("pushing %d\n", c->s->mine);
-    push(head, c);
-  }
-  return;
-}
-
-void score_game_tree (struct node * head) {
-  // keep two stacks -- one to keep track of those that need to be
-  // scored but can't (level below doesn't have scores), and a lower
-  // stack that can be scored, those with children with scores.
-  printf("** in score_game_tree **\n");
-  struct stack_node *upper_stack_head = init_stack();
-  struct stack_node *lower_stack_head = init_stack();
-  printf("initialized stacks\n");
-  push(upper_stack_head, head);
-  printf("pushed head\n");
-  while (!stack_is_empty(upper_stack_head)) {
-    struct node *upper_peek = peek(upper_stack_head);
-    printf("%d\n", upper_peek->s->mine);
-    printf("peeked at stack\n");
-    if (children_scored(upper_peek)) {
-      printf("in if\n");
-      upper_peek->s->score = score_children(upper_peek);
-      pop(upper_stack_head);
-    }
-    else {
-      printf("in else\n");
-      push_children(upper_stack_head, upper_peek);
-    }
-  }
 }
 
 int main () {
   printf("Shall we play a game?\n");
   unsigned int p1, p2;
   unsigned int m;
-  int x = 261; int y = 0;
+  int x = 5; int y = 256;
+  //int x = 261; int y = 0;
+  /* int x = 341; int y = 170; */
   struct node * head = build_game_tree(&x, &y);
   printf("built tree\n");
-  score_game_tree(head);
+  /* score_game_tree(head); */
   //printf("%d", endgame(&x, &y));
   /* while (!endgame(&p1, &p2)) { */
   /*   turn(&p1, &p2, &m); */
